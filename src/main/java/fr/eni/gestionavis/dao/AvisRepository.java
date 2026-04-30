519c0ef132f3952926b5921ebf44ea5ca2be1516
jbarlet2025 <barletjb.perso@gmail.com>
2026-04-30 14:08:17 +0200

    Renam path avis

--- src/main/java/fr/eni/gestionavis/dao/AvisRepository.java
+++ src/main/java/fr/eni/gestionavis/dao/AvisRepository.java
@@ -2,6 +2,8 @@ package fr.eni.gestionavis.dao;
 
 import fr.eni.gestionavis.bo.Avis;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "avis", path = "avis")
 public interface AvisRepository extends MongoRepository<Avis,String> {
 }
